a3beea04aaa784aee93c7b2968542f4a1f8a9aa7
rezxzer <rezorezorezo123321@gmail.com>
2025-04-28 00:00:44 +0400

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-gradient-to-r from-purple-500 to-blue-500 text-white p-4 text-center text-2xl font-bold shadow-md">
+      MetaFeed
+    </header>
+  );
+}
