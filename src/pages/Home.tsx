fc7440eec7716debc084c0e2e643127b8fb0d56f
rezxzer <rezorezorezo123321@gmail.com>
2025-04-27 23:31:10 +0400

    Create Home.tsx page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-4xl font-bold">Welcome to MetaFeed!</h1>
+    </div>
+  );
+}
