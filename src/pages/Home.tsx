64dc39f2ea3a32c277d78574b6201f36f7ec4b63
rezxzer <rezorezorezo123321@gmail.com>
2025-04-28 00:05:30 +0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
+import Header from '../components/Header';
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1 className="text-4xl font-bold">Welcome to MetaFeed!</h1>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow flex items-center justify-center">
+        <h1 className="text-4xl font-bold">Welcome to MetaFeed!</h1>
+      </main>
     </div>
   );
 }
