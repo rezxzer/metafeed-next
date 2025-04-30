55b4d7cb80d1a1ab7aa2708ecf0e71454ed40184
rezxzer <rezorezorezo123321@gmail.com>
2025-04-30 19:58:57 +0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,11 @@
 import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
 
 export default function Home() {
   return (
     <div className="bg-[#0d0b1f] min-h-screen text-white">
       <Header />
-      <main className="p-6">
-        <h1 className="text-4xl font-bold mt-10">Welcome to MetaFeed</h1>
-      </main>
+      <HeroSection />
     </div>
   );
 }
